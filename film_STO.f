5ecf7e63e0d06ce57a57be5b40bcf63b5a109ed2
luis.gc.rego <luis.gc.rego@477fdf95-05fb-44df-9e1b-6709a640fe8c>
2012-04-28 14:07:49 +0000

    gaussian cube files for MO coefficients

--- film_STO.f
+++ film_STO.f
@@ -33,19 +33,180 @@
  character(*) , optional   , intent(in) :: el_hl
 
 ! local variables ...
-complex*16 , allocatable :: C_bra(:), C_ket(:)
+ integer                  :: many_steps   = 131
+ integer                  :: medium_steps = 89
+ integer                  :: few_steps    = 59  
+ integer                  :: n_xyz_steps(3)
+ complex*16               :: TotalPsiKet
+ real*8     , allocatable :: xyz(:,:) , Psi(:,:,:)
+ real*8                   :: x , y , z , x0 , y0 , z0 , dx , dy , dz , a , b , c , r , SlaterOrbital
+ integer                  :: AtNo , i , j , ix , iy , iz , k 
+ character(len=2)         :: prefix
+ character(len=4)         :: string 
+ character(len=21)        :: f_name
 
-allocate( C_bra(size(bra)) )
-allocate( C_ket(size(ket)) )
+ real*8 , parameter :: aB = 0.529d0   ! <== Bohr radius
 
-C_bra = cmplx(bra)
-C_ket = cmplx(ket)
+ allocate(xyz(unit_cell%atoms,3))
 
-CALL gaussian_cube_format_Cmplx( C_bra , C_ket , it , t , el_hl )
+ write(string,'(i4.4)') it
+ prefix = merge( "el" , el_hl , .NOT. present(el_hl) )
+ f_name = prefix//'_MO_shot'//string//'.cube'
+ OPEN(unit=4,file=f_name,status='unknown')  
 
-deallocate( C_bra , C_ket )
+! bounding box for isosurfaces ... 
+ CALL BoundingBox( unit_cell ) 
 
-end subroutine gaussian_cube_format_Real
+! fix number of steps for each direction according to aspect ratio ... 
+ do i = 1 , 3
+
+    ! default case
+    n_xyz_steps(i) = medium_steps
+
+    If( unit_cell%BoxSides(i) >= 2.5d1 ) n_xyz_steps(i) = many_steps
+
+    If( unit_cell%BoxSides(i) <= 1.2d1 ) n_xyz_steps(i) = few_steps
+
+ end do
+
+!  voxel dimensions
+ dx = unit_cell%BoxSides(1) / n_xyz_steps(1) 
+ dy = unit_cell%BoxSides(2) / n_xyz_steps(2) 
+ dz = unit_cell%BoxSides(3) / n_xyz_steps(3)      
+
+!  translation to the center of mass
+ forall(i=1:unit_cell%atoms,j=1:3) xyz(i,j) = unit_cell%coord(i,j) - unit_cell%Center_of_Mass(j)
+
+! initial corner of the volume Box 
+ a = minval(xyz(:,1)) - fringe / two
+ b = minval(xyz(:,2)) - fringe / two
+ c = minval(xyz(:,3)) - fringe / two
+
+!  start writing Gaussian cube files 
+ write(4,*) System_Characteristics
+ write(4,*) 'initial_state = ',initial_state,'  /  time = ', t
+
+ write(4,111) unit_cell%atoms , a/aB , b/aB , c/aB
+ write(4,111) n_xyz_steps(1) + 1 , dx/aB   , 0.d0 , 0.d0 
+ write(4,111) n_xyz_steps(2) + 1 , 0.d0 , dy/aB   , 0.d0
+ write(4,111) n_xyz_steps(3) + 1 , 0.d0 , 0.d0 , dz/aB
+
+ DO i = 1 , unit_cell%atoms
+ 
+    write(4,113) unit_cell%AtNo(i), 0.0 , xyz(i,1)/aB , xyz(i,2)/aB , xyz(i,3)/aB
+
+ END DO
+
+!---------------------------------------------------------    
+!  drawing the wavefunction denssity in cube format
+!========================================================
+!  the order orbitals are stored
+! 
+!       S      -->  1   --> l = 0  ,  m =  0           
+!       Py     -->  2   --> l = 1  ,  m = -1    
+!       Pz     -->  3   --> l = 1  ,  m =  0         
+!       Px     -->  4   --> l = 1  ,  m = +1
+!       Dxy    -->  5   --> l = 2  ,  m = -2      
+!       Dyz    -->  6   --> l = 2  ,  m = -1
+!       Dz2    -->  7   --> l = 2  ,  m =  0     
+!       Dxz    -->  8   --> l = 2  ,  m = +1        
+!       Dx2y2  -->  9   --> l = 2  ,  m = +2        
+!========================================================
+
+ allocate( Psi(n_xyz_steps(1)+1,n_xyz_steps(2)+1,n_xyz_steps(3)+1) , source = D_zero ) 
+
+!$OMP parallel private(ix,iy,iz,x,y,z,x0,y0,z0,SlaterOrbital,r,AtNo,k,j,TotalPsiKet)
+!$OMP single
+ DO ix = 0 , n_xyz_steps(1)
+    x = a + ix * dx
+    
+    !$OMP task untied
+    DO iy = 0 , n_xyz_steps(2)
+        y = b + iy * dy
+ 
+    DO iz = 0 , n_xyz_steps(3) 
+        z = c + iz * dz
+
+        i = 0 
+        TotalPsiKet = C_zero 
+        DO k = 1 , unit_cell%atoms
+
+            AtNo = unit_cell%AtNo(k)
+
+            ! distance from the center of the nuclei
+
+            r = dsqrt((x-xyz(k,1))*(x-xyz(k,1)) + (y-xyz(k,2))*(y-xyz(k,2)) + (z-xyz(k,3))*(z-xyz(k,3))) 
+
+            ! coordinates centered on the nuclei   
+
+            x0 = x - xyz(k,1) 
+            y0 = y - xyz(k,2) 
+            z0 = z - xyz(k,3) 
+
+            do j = 1 , atom(AtNo)%DOS
+                i = i + 1
+                select case (j)
+                    case( 1 ) 
+                        SlaterOrbital = s_orb(r,AtNo)  
+                    case( 2 ) 
+                        SlaterOrbital = p_orb(r,y0,AtNo)
+                    case( 3 ) 
+                        SlaterOrbital = p_orb(r,z0,AtNo)
+                    case( 4 ) 
+                        SlaterOrbital = p_orb(r,x0,AtNo)
+                    case( 5 ) 
+                        SlaterOrbital = d_xyz(r,x0,y0,AtNo)
+                    case( 6 ) 
+                        SlaterOrbital = d_xyz(r,y0,z0,AtNo)
+                    case( 7 ) 
+                        SlaterOrbital = d_z2(r,x0,y0,z0,AtNo)
+                    case( 8 ) 
+                        SlaterOrbital = d_xyz(r,x0,z0,AtNo)
+                    case( 9 ) 
+                        SlaterOrbital = d_x2y2(r,x0,y0,AtNo)
+                end select
+                TotalPsiKet = TotalPsiKet + ket(i) * SlaterOrbital
+            end do  ! <== DOS
+        end do  ! <== atoms
+
+        Psi( ix+1 , iy+1 , iz+1 ) = TotalPsiKet
+
+    END DO          ! <==  Z coord
+    END DO          ! <==  Y coord
+    !$OMP end task 
+ END DO             ! <==  X coord
+!$OMP end single 
+!$OMP end parallel 
+
+!::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+ DO ix = 0 , n_xyz_steps(1)
+ x = a + ix * dx
+ 
+    DO iy = 0 , n_xyz_steps(2)
+    y = b + iy * dy
+ 
+        DO iz = 0 , n_xyz_steps(3) 
+        z = c + iz * dz
+
+            write(4,112,advance='no') Psi( ix+1 , iy+1 , iz+1 ) 
+
+            If( (mod(iz,6) == 5) ) write(4,'(a)',advance='yes') 
+
+        END DO                                                       
+        write(4,'(a)',advance='yes') 
+    END DO                                                      
+ END DO                                                     
+
+!::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+ close(4)
+
+ deallocate(xyz , Psi)
+
+ include 'formats.h'
+
+ end subroutine Gaussian_Cube_Format_Real
 !
 !
 !
