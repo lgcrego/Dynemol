0f5f0fad13e2db9de56ee2acc6bffedd5f72048c
luis.gc.rego <luis.gc.rego@477fdf95-05fb-44df-9e1b-6709a640fe8c>
2013-04-04 21:47:47 +0000

    maintenance

--- types_MM.f
+++ types_MM.f
@@ -1,29 +1,5 @@
 module MM_types
 
-    type MM_molecular
-        integer                             :: my_species
-        integer                             :: N_of_atoms
-        integer                             :: N_of_molecules
-        real*8                              :: cm(3)
-        real*8                              :: mass
-        logical                             :: flex
-        character(3)                        :: residue
-        integer                             :: nr
-        integer                             :: Nbonds
-        integer             , allocatable   :: bonds(:,:)
-        real*8              , allocatable   :: kbond0(:,:)
-        integer                             :: Nangs
-        integer             , allocatable   :: angs(:,:)
-        real*8              , allocatable   :: kang0(:,:)
-        integer                             :: Ndiheds
-        integer             , allocatable   :: diheds(:,:)
-        real*8              , allocatable   :: kdihed0(:,:)
-        integer                             :: Nharm
-        integer                             :: Nbonds14
-        integer             , allocatable   :: bonds14(:,:)
-        real*8              , allocatable   :: fact14(:)
-    end type MM_molecular
-
     type MM_atomic
         integer                             :: AtNo
         integer                             :: my_id
@@ -50,14 +26,52 @@ module MM_types
         logical                             :: free
     end type MM_atomic
 
+    type MM_molecular
+        type(MM_atomic)     , allocatable   :: atom(:)
+        integer                             :: my_species
+        integer                             :: N_of_atoms
+        integer                             :: N_of_molecules
+        real*8                              :: cm(3)
+        real*8                              :: mass
+        logical                             :: flex
+        character(3)                        :: residue
+        integer                             :: nr
+        integer                             :: Nbonds
+        integer             , allocatable   :: bonds(:,:)
+        real*8              , allocatable   :: kbond0(:,:)
+        integer                             :: Nangs
+        integer             , allocatable   :: angs(:,:)
+        real*8              , allocatable   :: kang0(:,:)
+        integer                             :: Ndiheds
+        integer             , allocatable   :: diheds(:,:)
+        real*8              , allocatable   :: kdihed0(:,:)
+        integer                             :: Nharm
+        integer                             :: Nbonds14
+        integer             , allocatable   :: bonds14(:,:)
+        real*8              , allocatable   :: fact14(:)
+    end type MM_molecular
+
     type MM_system
+        type(MM_atomic)     , allocatable   :: atom(:)
+        type(MM_molecular)  , allocatable   :: molecule(:)        
         real*8                              :: box(3)
         real*8                              :: ibox(3)
         integer                             :: N_of_atoms
         integer                             :: N_of_species
         integer                             :: N_of_molecules
+        integer                             :: N_of_AtomTypes        
         integer                             :: CombinationRule
     end type MM_system
+
+    type DefineBonds
+        character(15)                       :: nome
+        real*8                              :: kbond0(2)
+    end type DefineBonds
+
+    type DefineAngles
+        character(15)                       :: nome
+        real*8                              :: kang0(2)
+    end type DefineAngles
 
 end module MM_types
 !
