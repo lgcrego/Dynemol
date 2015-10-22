636fd260a337983b9c5c62428e4c06d363e310fe
lgcrego <lgcrego@477fdf95-05fb-44df-9e1b-6709a640fe8c>
2015-10-22 11:47:01 +0000

    upgrade on subroutine to include expanded charge combinations and exclusion length
    Former-commit-id: 912b904764e156c2aaf8f53ddee4ed383e1c68f1

--- CoulInt_QMMM.f
+++ CoulInt_QMMM.f
@@ -1,13 +1,26 @@
 module QMMM_m
    
     use constants_m
-    use MD_read_m           , only : atom , MM
-    use polarizability_m    , only : Induced_DP
-
-    private
+    use for_force            , only : pot_total
+    use MD_read_m            , only : atom , MM
+    use polarizability_m     , only : Induced_DP
+    use Semi_Empirical_Parms , only : chemical_element => atom
 
     public :: QMMM_FORCE
 
+    private
+
+    ! module parameters ...
+    real*8  , parameter :: c0 = 1.5d0               ,&   ! 3/2
+                           c1 = 8.888888888888d-2   ,&   ! 4/45
+                           c2 = 7.5d0               ,&   ! 15/2
+                           c3 = 15.d0               ,&
+                           c4 = 22.5d0              ,&   ! 45/2
+                           c5 = 11.25d0             ,&   ! 45/4
+                           c6 = 6.349206d-3         ,&   ! 2/315
+                           c7 = 2.821869d-4         ,&   ! 4/14175
+                           c8 = 85511197d-6         ,&   ! 4/467775
+                           c0_inv = 1.d0/c0
 contains
 !
 !
@@ -21,7 +34,7 @@ real*8  , intent(in)    :: NetCharge(:)
 ! local variables ...
 real*8  , dimension (3) :: rij , a(3) , f(3)
 real*8                  :: rijq , rijsq , freal 
-real*8                  :: V_ChgChg , V_ChgIndDP
+real*8                  :: V_ChgChg , V_ChgIndDP , NetChrg_i , NetChrg_j , TotalChrg_i, ChargeProduct_ij
 integer                 :: i , j
 
 ! local parameters ...
@@ -46,15 +59,21 @@ do i = 1 , size(atom)
         rijq = sum( rij(:) * rij(:) )
         rijsq = sqrt( rijq )
 
-        freal = coulomb * NetCharge(i) * NetCharge(j) * ( D_ONE / rijq  ) * ( D_ONE / rijsq )
+        NetChrg_i = NetCharge(i) !* exclusion( i , rijsq*HALF )
+        NetChrg_j = NetCharge(j) !* exclusion( j , rijsq*HALF )
+
+        ChargeProduct_ij = NetChrg_i * NetChrg_j 
+        ChargeProduct_ij = ChargeProduct_ij + atom(i)%charge*NetChrg_j + atom(j)%charge*NetChrg_i
+
+        freal = coulomb * ChargeProduct_ij * ( D_ONE / rijq  ) * ( D_ONE / rijsq )
 
         atom(i) % fcoupling(1:3) = atom(i) % fcoupling(1:3) + freal * rij(1:3)
         atom(j) % fcoupling(1:3) = atom(j) % fcoupling(1:3) - freal * rij(1:3)
 
-        V_ChgChg = V_ChgChg + coulomb * NetCharge(i) * NetCharge(j) * ( D_ONE / rijsq )
+        V_ChgChg = V_ChgChg + coulomb * ChargeProduct_ij * ( D_ONE / rijsq )
 
     end do
-
+    
     ! charge/induced-dipole interaction ...
     do j = 1 , size(atom)
 
@@ -72,24 +91,105 @@ do i = 1 , size(atom)
             ! a = 3*(p.r)*r / ( |r|^2 ) - p ...
             a(1:3) = THREE * a(1:3) * ( D_ONE / rijq ) - Induced_DP(j,1:3)
 
-            f(1:3) = coulomb * NetCharge(i) * a(1:3) * ( D_ONE / rijq ) * ( D_ONE / rijsq ) 
+            TotalChrg_i = atom(i)%charge + NetCharge(i)
 
-            atom(i) % fcoupling(1:3) = atom(i) % fcoupling(1:3) + f(1:3)
-            atom(j) % fcoupling(1:3) = atom(j) % fcoupling(1:3) - f(1:3)
+            f(1:3) = coulomb * TotalChrg_i * a(1:3) * ( D_ONE / rijq ) * ( D_ONE / rijsq ) 
+
+            atom(i)% fcoupling(1:3) = atom(i)% fcoupling(1:3) + f(1:3)
+            atom(j)% fcoupling(1:3) = atom(j)% fcoupling(1:3) - f(1:3)
 
             V_ChgIndDP = V_ChgIndDP &
-                       + coulomb*D_2_eAngs*NetCharge(i)*dot_product(Induced_DP(j,1:3),rij(1:3))*( D_ONE / rijq )*( D_ONE / rijsq )
+                       + coulomb*D_2_eAngs*TotalChrg_i*dot_product(Induced_DP(j,1:3),rij(1:3))*( D_ONE / rijq )*( D_ONE / rijsq )
 
         end if
     end do
 end do
 
-! New Get total force ...
+! Append total force with Excited State Coulombic terms; force units = J/mts = Newtons ...
 do i = 1 , MM % N_of_atoms
-    atom(i) % ftotal(:) = atom(i) % ftotal(:) + atom(i) % fcoupling(:) * 1.d-10
+    atom(i)% ftotal(:) = atom(i)% ftotal(:) + atom(i)% fcoupling(:) * Angs_2_mts
 end do
 
+pot_total = pot_total + (V_ChgChg + V_ChgIndDP ) * mol * micro * factor3 / MM%N_of_molecules 
+
 end subroutine QMMM_FORCE
+!
+!
+!
+!===========================
+ function exclusion( i , r )
+!===========================
+implicit none
+integer , intent(in) :: i
+real*8  , intent(in) :: r
+
+! local variables ...
+integer :: N_level
+real*8  :: a, a2, a3, a4, a5, a6, a7, a9, a11
+real*8  :: r2, r3, r4, r5, r6, r7, r9, r11
+real*8  :: exclusion
+
+! N quantum number of s orbital ...
+N_level = chemical_element( atom(i)%AtNo ) % NQuant(0)
+
+! zeta parameter of s orbital ...
+a = chemical_element( atom(i)%AtNo ) % zeta(0,1)
+print*, a
+r2 = r*r
+a2 = a*a
+
+select case ( N_level )
+
+    case( 1 )
+
+        exclusion = -two*a2*exp(-two*r*a)*( r2 + r/a + D_one/(two*a2) ) + D_one 
+
+    case( 2 )
+
+        r3 = r2*r ; r4 = r2*r2
+        
+        a3 = a2*a ; a4 = a2*a2
+
+        exclusion = -c0_inv*a4*exp(-two*r*a) * (r4 + two*r3/a + three*r2/a2 + three*r/a3 + c0/a4) + D_one
+
+    case( 3 )
+
+        r3 = r2*r ; r4 = r2*r2 ; r5 = r4*r ; r6 = r3*r3
+
+        a3 = a2*a ; a4 = a2*a2 ; a5 = a4*a ; a6 = a3*a3
+
+        exclusion = -c1*a6*exp(-two*r*a) * (r6 + three*r5/a + c2*r4/a2 + c3*r3/a3 + c4*r2/a4 + c4*r/a5 + c5/a6) + D_one
+
+    case( 4 )
+
+        r3 = r2*r ; r4 = r2*r2 ; r5 = r4*r ; r6 = r3*r3 ; r7 = r3*r4 
+
+        a3 = a2*a ; a4 = a2*a2 ; a5 = a4*a ; a6 = a3*a3 ; a7 = a3*a4 
+
+        exclusion = -c1*a6*exp(-two*r*a) * (r6 + three*r5/a + c2*r4/a2 + c3*r3/a3 + c4*r2/a4 + c4*r/a5 + c5/a6) + D_one &
+                    -c6*r7*a7*exp(-two*r*a) * (four + a*r)
+
+    case( 5 ) 
+
+        r3 = r2*r ; r4 = r2*r2 ; r5 = r4*r ; r6 = r3*r3 ; r7 = r3*r4 ; r9 = r4*r5
+
+        a3 = a2*a ; a4 = a2*a2 ; a5 = a4*a ; a6 = a3*a3 ; a7 = a3*a4 ; a9 = a4*a5
+
+        exclusion = -c1*a6*exp(-two*r*a) * (r6 + three*r5/a + c2*r4/a2 + c3*r3/a3 + c4*r2/a4 + c4*r/a5 + c5/a6) + D_one &
+                    -( c6*r7*a7*(four + a*r) + c7*r9*a9*(five + r*a) ) * exp(-two*r*a)
+
+    case( 6 ) 
+
+        r3 = r2*r ; r4 = r2*r2 ; r5 = r4*r ; r6 = r3*r3 ; r7 = r3*r4 ; r9 = r4*r5 ; r11 = r9*r2
+
+        a3 = a2*a ; a4 = a2*a2 ; a5 = a4*a ; a6 = a3*a3 ; a7 = a3*a4 ; a9 = a4*a5 ; a11 = a9*a2
+
+        exclusion = -c1*a6*exp(-two*r*a) * (r6 + three*r5/a + c2*r4/a2 + c3*r3/a3 + c4*r2/a4 + c4*r/a5 + c5/a6) + D_one &
+                    -( c6*r7*a7*(four + a*r) + c7*r9*a9*(five + r*a) + c8*r11*a11*(six + r*a) ) * exp(-two*r*a)
+
+end select 
+
+end function exclusion
 !
 !
 !
