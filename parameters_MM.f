065341e46714622baa4218265495da5f4641b23e
lgcrego <lgcrego@477fdf95-05fb-44df-9e1b-6709a640fe8c>
2018-01-26 12:36:06 +0000

    maintenance
    Former-commit-id: 93c627d65a3104e885d3f9ea2867d6526fffcd33

--- parameters_MM.f
+++ parameters_MM.f
@@ -37,11 +37,13 @@ implicit none
 !------------------------------------------------------------------------------
 ! repeat the following information filling for all the different species ...
 !
-  species(1) % residue         = "BZN"      ! <== Residue label for species i ; character(len3)
+  species(1) % residue         = "PSB"      ! <== Residue label for species i ; character(len3)
   species(1) % N_of_molecules  = 1          ! <== Number of molecules of species i
-  species(1) % N_of_atoms      = 12         ! <== Number of atoms comprosing a single molecule of species i
+  species(1) % N_of_atoms      = 66         ! <== Number of atoms comprosing a single molecule of species i
   species(1) % flex            = T_         ! <== Flexible : T_ , F_
   
+  Selective_Dynamics = F_                   ! <== ad_hoc_MM_tuning sets MegaMass to selected atoms
+
 !------------------------------------------------------------------------------
 ! ENVIRONMENT parameters ...
 !
@@ -58,7 +60,7 @@ implicit none
                                                 ! <== SMALL = STRONG ; use "= infty" to decouple
 
   cutoff_radius             = 15.d0             ! <== Cut off radius (Angs.) for electrostatic and LJ interactions
-  damping_Wolf              = 0.2d0             ! <== damping parameter (Angs.^-1) ; reasonable values: R_c*Wolf ~ ....
+  damping_Wolf              = 0.02d0            ! <== damping parameter (Angs.^-1) ; reasonable values: R_c*Wolf ~ ....
                                                 ! <== Wolf's method damping parameter (length^{-1}) ; (J. Chem. Phys. 1999; 110(17):8254)
 !------------------------------------------------------------------------------
 ! GENERAL INFO ...
@@ -66,10 +68,8 @@ implicit none
 
   driver_MM              = "MM_Dynamics"       ! <== MM_Dynamics , MM_Optimize , NormalModes , Parametrize
 
-  read_velocities        = F_                   ! <== reads the initial velocities : T_ , F_
+  read_velocities        = T_                   ! <== reads the initial velocities : T_ , F_
   gmx_input_format       = T_                   ! <== reads FF parameters from gmx input files : T_ , F_  
-
-  Selective_Dynamics     = F_                   ! <== ad_hoc_MM_tuning sets MegaMass to selected atoms
 
   MM_log_step            =  100                 ! <== step for saving MM results & parameters
   MM_frame_step          =  20                  ! <== step for saving MM results & parameters
