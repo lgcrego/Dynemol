6c7fca50442d8b94b473d358ead3514cae1d6b16
luisgcrego <luis.gc.rego@gmail.com>
2025-02-03 16:22:11 -0300

    maintenance

--- diagnostic.f
+++ diagnostic.f
@@ -105,19 +105,6 @@ CALL Read_Command_Lines_Arguments( MOnum )
 
  If( HFP_Forces ) CALL HuckelForces( Extended_Cell, ExCell_basis, UNI )
 
-Print*, " " 
-Print 10, "dE1 = ",UNI%erg(32) - UNI%erg(31) , "  vs " , 2.74 , "  => error = ", ( UNI%erg(32) - UNI%erg(31)) - 2.74
-Print 10, "dE2 = ",UNI%erg(32) - UNI%erg(30) , "  vs " , 3.78 , "  => error = ", ( UNI%erg(32) - UNI%erg(30)) - 3.78
-Print 10, "dE3 = ",UNI%erg(32) - UNI%erg(29) , "  vs " , 3.87 , "  => error = ", ( UNI%erg(32) - UNI%erg(29)) - 3.87
-Print 10, "dE3 = ",UNI%erg(32) - UNI%erg(28) , "  vs " , 3.87 , "  => error = ", ( UNI%erg(32) - UNI%erg(28)) - 3.87
-Print 10, "dE1 = ",UNI%erg(33) - UNI%erg(31) , "  vs " , 4.65 , "  => error = ", ( UNI%erg(33) - UNI%erg(31)) - 4.65
-Print 10, "dE2 = ",UNI%erg(34) - UNI%erg(31) , "  vs " , 4.65 , "  => error = ", ( UNI%erg(34) - UNI%erg(31)) - 4.65
-Print 10, "dE3 = ",UNI%erg(31) - UNI%erg(30) , "  vs " , 1.23 , "  => error = ", ( UNI%erg(31) - UNI%erg(30)) - 1.23
-Print 10, "dE1 = ",UNI%erg(31) - UNI%erg(29) , "  vs " , 1.23 , "  => error = ", ( UNI%erg(31) - UNI%erg(29)) - 1.23
-Print 10, "dE2 = ",UNI%erg(31) - UNI%erg(28) , "  vs " , 1.23 , "  => error = ", ( UNI%erg(31) - UNI%erg(28)) - 1.23
- 
-10 format(A6,F9.5,A5,F9.5,A13,F9.5)
-
  If( GaussianCube .AND. (size(MOnum) > 0) ) then
 
      ! use this to check the orbitals separately ... 
@@ -253,7 +240,7 @@ do i = 1 , size(MOnum)
      do k = 1 , size(list)
           write(18,"(a5)",advance='no') list(k) 
           do j = 1 , 9     
-               pop(j) = Mulliken( UNI , basis , MO=MOnum(i) , AO = AO_orbs(j) , EHSymbol=list(k) )
+               pop(j) = Mulliken( UNI , basis , MO=MOnum(i) , AO = AO_orbs(j) , EHSymbol=list(k) , Symbol="XX" , residue="XXX" )
           end do
      write(18,"(9F10.4)") (pop(j) , j=1,9)
      end do
@@ -382,7 +369,8 @@ select case (token)
            do i = 1 , size(UNI%erg)
                 do j = 1 , 9     
                      do k = 1 , size(list)
-                          PDOS(i,k) = PDOS(i,k) + Mulliken( UNI , basis , MO=i , AO = AO_orbs(j) , EHSymbol=list(k) )
+                          PDOS(i,k) = PDOS(i,k) &
+                                    + Mulliken( UNI , basis , MO=i , AO=AO_orbs(j) , EHSymbol=list(k) , Symbol="XX" , residue="XXX" )
                      end do
                 end do
                 !write(*,"(t1,i3,t6,F12.4,F10.5,5F10.3)") i , UNI%erg(i) , sum(PDOS(i,:)) , PDOS(i,:)
@@ -392,7 +380,8 @@ select case (token)
            do i = 1 , size(UNI%erg)
                 do j = 1 , 9     
                      do k = 1 , size(list)
-                          PDOS(i,k) = PDOS(i,k) + Mulliken( UNI , basis , MO=i , AO = AO_orbs(j) , Symbol=list(k) )
+                          PDOS(i,k) = PDOS(i,k) & 
+                                    + Mulliken( UNI , basis , MO=i , AO=AO_orbs(j) , EHSymbol="XX" , Symbol=list(k) , residue="XXX" )
                      end do
                 end do
                 !write(*,"(t1,i3,t6,F12.4,F10.5,5F10.3)") i , UNI%erg(i) , sum(PDOS(i,:)) , PDOS(i,:)
