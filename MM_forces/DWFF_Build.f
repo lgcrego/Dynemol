7b04c6514189ba6e5acb4c235a7053c229e0fa22
luisgcrego <luis.gc.rego@gmail.com>
2026-06-27 20:32:50 -0300

    left behind from previous commit

--- MM_forces/DWFF_Build.f
+++ MM_forces/DWFF_Build.f
@@ -2,11 +2,12 @@ module Build_DWFF
 
 use iso_fortran_env
 use constants_m
-use type_m      , only: dynemoldir, warning
-use util_m      , only: TO_UPPER_CASE, split_line
-use MM_types    , only: Dissociative, MM_molecular, MM_atomic
+use MM_parms_module , only: DWFF_type
+use type_m          , only: dynemoldir, warning
+use util_m          , only: TO_UPPER_CASE, split_line
+use MM_types        , only: Dissociative, MM_molecular, MM_atomic
 
-    public :: include_DWFF_parameters , HOH_diss_parms, read_DWFF_parameters, dump_DWFF_parameters, check_DWFF_parameters
+    public :: include_DWFF_parameters , HOH_diss_parms, dump_DWFF_parameters, set_H_pointers
 
     private
 
@@ -30,26 +31,42 @@ use MM_types    , only: Dissociative, MM_molecular, MM_atomic
 contains
 !
 !
-!===========================================
- subroutine include_DWFF_parameters(species)
-!===========================================
+!==================================
+ subroutine include_DWFF_parameters
+!==================================
 implicit none
-type(MM_molecular), intent(in):: species(:)
 
-call DWFF_Parameters
+!local variables ...
+character(len=8) :: which_DWFF
 
-call get_DWFF_Parameters
+which_DWFF = TO_UPPER_CASE(trim(adjustl(DWFF_type)))
 
-call set_H_pointers(species)
+select case (which_DWFF) 
+
+    case("DIFFUSE" , "DIFUSE")
+        call Diffuse_DWFF_Parameters
+
+    case("SPC_LIKE" , "SPC-LIKE")
+        call SPC_like_DWFF_Parameters
+
+    case("QMMM")
+!        call QMMM_DWFF_Parameters
+
+    case default
+        error stop "unknown choice for <use_DWFF_type> parameter: check your card.inpt file"
+
+end select
+
+call export_DWFF_Parameters
 
 end subroutine include_DWFF_parameters
 !
 !
 !
 !
-!===========================
- subroutine DWFF_Parameters 
-!===========================
+!==================================
+ subroutine Diffuse_DWFF_Parameters 
+!==================================
 implicit none
 !-------------------------------------------------------------
 !                PARAMETERS FROM THE ARTICLE
@@ -62,11 +79,11 @@ implicit none
 call define_mtxA 
 
 !  Charges on Species
-if(.not. from_outside(1)) PointCharge_O = -0.904d0
-if(.not. from_outside(2)) PointCharge_H = +0.452d0
+PointCharge_O = -0.904d0
+PointCharge_H = +0.452d0
 
-if(.not. from_outside(3)) DiffCharge_O  = +0.226d0
-if(.not. from_outside(4)) DiffCharge_H  = -0.113d0
+DiffCharge_O  = +0.226d0
+DiffCharge_H  = -0.113d0
 
 !  Parameters of the Two-Body Potential
 OH_A_repulsive = 2.283d-16  ! <== Joule
@@ -85,10 +102,10 @@ HH_q_dsprsion  = 24.d0      ! <== Angs
 HH_C6_dsprsion = D_zero     ! <== J*Angs^6
 
 ! Three-Body Parameters
-if(.not. from_outside(5)) lambda   = 3.0d-17 ! <== Joule
-if(.not. from_outside(6)) theta_0  = 100.d0  ! <== degrees
-if(.not. from_outside(7)) r0       = 1.6d0   ! <== Angs
-if(.not. from_outside(8)) gama     = 1.3d0   ! <== Angs
+lambda   = 3.0d-17 ! <== Joule
+theta_0  = 100.d0  ! <== degrees
+r0       = 1.6d0   ! <== Angs
+gama     = 1.3d0   ! <== Angs
 
 !-------------------------------------------------------------
 !          CONVERT PARAMETERS TO DYNEMOL UNITS     
@@ -110,7 +127,76 @@ HH_C6_dsprsion = HH_C6_dsprsion / factor3
 lambda  = lambda / factor3
 theta_0 = theta_0 * deg_2_rad   
 
-end subroutine DWFF_Parameters 
+end subroutine Diffuse_DWFF_Parameters 
+!
+!
+!
+!
+!===================================
+ subroutine SPC_like_DWFF_Parameters 
+!===================================
+implicit none
+!-------------------------------------------------------------
+!                PARAMETERS FROM THE ARTICLE
+!
+!       Towards a dissociative SPC-like water model: 
+! probing the impact of intramolecular Coulombic contributions
+!           Martin J. Wiedemair and Thomas S. Hofer
+!           Phys. Chem. Chem. Phys., 2017, 19, 31910
+!-------------------------------------------------------------
+
+call define_mtxA 
+
+!  Charges on Species
+PointCharge_O = -0.898d0
+PointCharge_H = +0.449d0
+
+DiffCharge_O  = D_zero
+DiffCharge_H  = D_zero
+
+!  Parameters of the Two-Body Potential
+OH_A_repulsive = 2.283d-16  ! <== Joule
+OH_short_range = PolynomialFunction( )       ! <== Angs
+OH_q_dsprsion  = 24.d0      ! <== Angs
+OH_C6_dsprsion = D_zero     ! <== J*Angs^6
+
+OO_A_repulsive = 4.250d-17  ! <== Joule
+OO_short_range = 0.610d0    ! <== Angs
+OO_q_dsprsion  = 24.d0      ! <== Angs
+OO_C6_dsprsion = 4.226d-18  ! <== J*Angs^6
+
+HH_A_repulsive = D_zero     ! <== Joule
+HH_short_range = D_zero     ! <== Angs
+HH_q_dsprsion  = 24.d0      ! <== Angs
+HH_C6_dsprsion = D_zero     ! <== J*Angs^6
+
+! Three-Body Parameters
+lambda   = 2.0d-17 ! <== Joule
+theta_0  = 98.09d0 ! <== degrees
+r0       = 1.55d0  ! <== Angs
+gama     = 1.19d0  ! <== Angs
+
+!-------------------------------------------------------------
+!          CONVERT PARAMETERS TO DYNEMOL UNITS     
+! the following values are being converted to Dynemol internal units ...                                                                       
+! Dynemol uses Joule and Newton units to evaluate the eqs. of motion ...
+!-------------------------------------------------------------
+
+!  Parameters of the Two-Body Potential
+OH_A_repulsive = OH_A_repulsive / factor3
+OH_C6_dsprsion = OH_C6_dsprsion / factor3
+
+OO_A_repulsive = OO_A_repulsive / factor3
+OO_C6_dsprsion = OO_C6_dsprsion / factor3
+
+HH_A_repulsive = HH_A_repulsive / factor3
+HH_C6_dsprsion = HH_C6_dsprsion / factor3
+
+! Three-Body Parameters
+lambda  = lambda / factor3
+theta_0 = theta_0 * deg_2_rad   
+
+end subroutine SPC_like_DWFF_Parameters 
 !
 !
 !
@@ -202,9 +288,9 @@ end  subroutine set_H_pointers
 !
 !
 !
-!===============================
- subroutine  get_DWFF_Parameters
-!===============================
+!==================================
+ subroutine  export_DWFF_Parameters
+!==================================
 implicit none
 
 ! atomic pairs
@@ -257,193 +343,26 @@ HOH_diss_parms% Angle(1,4) = cos(theta_0)
 HOH_diss_parms% PointCharge_O = PointCharge_O
 HOH_diss_parms% PointCharge_H = PointCharge_H
 
-end subroutine get_DWFF_Parameters
+end subroutine export_DWFF_Parameters
 !
 !
 !
-!======================================
- subroutine read_DWFF_parameters(funit)
-!======================================
+!=======================================
+ subroutine dump_DWFF_parameters(f_unit)
+!=======================================
 implicit none
-integer, intent(in):: funit
+integer, intent(in) :: f_unit
 
-!local variables
-character(len=120) :: line
-character(len=80)  :: string
-integer :: pos, fim, ioerr
-
-from_outside = .false.
-
-do 
-    read(funit,'(A)',iostat=ioerr) line
-
-    ! End-of-file
-    if (ioerr < 0) exit
-
-    ! Read error
-    if (ioerr > 0) then
-        print*, "Error reading DWFF parameter file"
-        stop
-    end if
-
-    ! Skip blank lines
-    if (len_trim(line) == 0) cycle
-
-    ! Skip comment lines
-    if (line(1:1) == "!") cycle
-
-    pos = index(line, "=")
-    if (pos == 0) then
-        print*, "Error parsing DWFF parameter file"
-        stop
-    end if
-
-    fim = index(line, "!")
-    if (fim == 0) fim = len_trim(line) + 1
-    
-    string = TO_UPPER_CASE( trim(adjustL(line(:pos-1))) )
-
-    ioerr = 0
-    select case(string)
-
-         case("POINTCHARGE_O")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  Pointcharge_O
-               from_outside(1) = .true.
-
-         case("POINTCHARGE_H")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  Pointcharge_H
-               from_outside(2) = .true.
-
-         case("DIFFCHARGE_O")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  Diffcharge_O
-               from_outside(3) = .true.
-
-         case("DIFFCHARGE_H")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  Diffcharge_H
-               from_outside(4) = .true.
-
-         case("LAMBDA")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  lambda
-               from_outside(5) = .true.
-
-         case("THETA_0","THETA-0")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  theta_0
-               from_outside(6) = .true.
-
-         case("R0")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  R0
-               from_outside(7) = .true.
-
-         case("GAMA","GAMMA")
-               read(line(pos+1:fim-1),*,iostat=ioerr)  gama
-               from_outside(8) = .true.
-
-        end select 
-
-        if (ioerr /= 0) then
-            print*, "Error parsing line:"
-            print*, trim(line)
-            stop
-        end if
-end do
-
-end subroutine read_DWFF_parameters
-!
-!
-!
-!===============================
- subroutine dump_DWFF_parameters
-!===============================
-implicit none
-
-!local variables
-character(len=:) , allocatable :: keyword_list(:), string(:)
-character(120)                 :: line
-integer                        :: f_unit, ioerr
-
-allocate( character(len=14) :: keyword_list(12) )
-
-keyword_list = [ "OH_A_repulsive", "OH_short_range", "OH_q_dsprsion", "OH_C6_dsprsion", &
-                 "OO_A_repulsive", "OO_short_range", "OO_q_dsprsion", "OO_C6_dsprsion", &
-                 "HH_A_repulsive", "HH_short_range", "HH_q_dsprsion", "HH_C6_dsprsion"  ]
-
-write(51,"(A18,F8.4)") "PointCharge_O  = ", PointCharge_O                                                                                        
-write(51,"(A18,F8.4)") "PointCharge_H  = ", PointCharge_H
-write(51,"(A18,F8.4)") "DiffCharge_O   = ", DiffCharge_O
-write(51,"(A18,F8.4)") "DiffCharge_H   = ", DiffCharge_H
-
-OPEN(newunit=f_unit, file=dynemoldir//'MM_forces/DWFF_Build.f', status='old')
-do
-    read(f_unit,'(A)',iostat=ioerr) line
-
-    ! end-of-file
-    if (ioerr < 0) exit
-
-    ! read error
-    if (ioerr > 0) then
-        print*, "Error reading file: DWFF_Build.f"
-        close(f_unit)
-    end if
-    
-    allocate( string , source=split_line( line , token_length=14 ) )
-
-    if (.not. allocated(string)) cycle
-
-    if (size(string) > 0) then
-        if (any(keyword_list == string(1))) then
-            write(51,*) trim(line)
-        end if
-
-        if (trim(string(1)) == "HH_C6_dsprsion") then
-            deallocate(string)
-            exit
-        end if
-    end if
-
-    deallocate(string)
-
-end do
-close(f_unit)
-
+write(51,"(A18,F8.4)")   "PointCharge_O  = ", PointCharge_O                                                                                        
+write(51,"(A18,F8.4)")   "PointCharge_H  = ", PointCharge_H
+write(51,"(A18,F8.4)")   "DiffCharge_O   = ", DiffCharge_O
+write(51,"(A18,F8.4)")   "DiffCharge_H   = ", DiffCharge_H
 write(51,"(A20,ES10.3)") "lambda  (Joule)  = ", lambda * factor3
-write(51,"(A20,F8.4)") "theta_0 (degree) = ", theta_0 / deg_2_rad
-write(51,"(A20,F8.4)") "r0 (Angstron)    = ", r0
-write(51,"(A20,F8.4)") "gamma (Angstron) = ", gama
+write(51,"(A20,F8.4)")   "theta_0 (degree) = ", theta_0 / deg_2_rad
+write(51,"(A20,F8.4)")   "r0 (Angstron)    = ", r0
+write(51,"(A20,F8.4)")   "gamma (Angstron) = ", gama
 
 end subroutine dump_DWFF_parameters
-!
-!
-!
-!===================================
-subroutine check_DWFF_parameters(a)
-!===================================
-implicit none
-type(MM_atomic), intent(in) :: a(:)
-
-! Local variables
-logical :: flag(size(a))
-real(8), parameter :: tol = 1.d-10
-
-flag = .false.
-
-!-------------------------------------------------
-! Check MM charges for DWFF atom types
-!-------------------------------------------------
-where (a%MMSymbol == "OX")
-    flag = abs(a%MM_charge - PointCharge_O) > tol
-elsewhere (a%MMSymbol == "HX")
-    flag = abs(a%MM_charge - PointCharge_H) > tol
-end where
-
-!-------------------------------------------------
-! Abort if inconsistencies are detected
-!-------------------------------------------------
-if (any(flag)) then
-    call warning("halting: HOH.psf file has conflicting MM charges")
-    stop
-end if
-
-end subroutine check_DWFF_parameters
 !
 !
 !
